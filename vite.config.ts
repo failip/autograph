2bc4b1931beafc2fbbee099c84b23ad37fc3fd31
Philipp Kuboth <philipp@kuboth.dev>
2024-12-17 12:31:15 +0100

    Remove unused proxying

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		proxy: {
-			'/graph/': {
-				target: "http://localhost:43070",
-				// target: "http://10.42.144.124:43070",
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/graph/, ''),
-				secure: false
-			},
-		},
 		watch: {
 			ignored: ["**/static/**", "build/**"],
 		},
