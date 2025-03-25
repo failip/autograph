b08b0b997e9f0f26cd8d7c8ce12ab1cec27002e9
Philipp Kuboth <philipp@kuboth.dev>
2025-03-25 16:57:55 +0100

    Connect to database (#8)

---
+++ src/routes/database/+page.svelte
@@ -0,0 +1,155 @@
+<script lang="ts">
+import GraphElement from "$lib/graphs/Graph.svelte";
+import { createGraphFromString } from "$lib/graphs/graphs";
+import type { Graph } from "ngraph.graph";
+let loadedGraph = false;
+let graph: Graph;
+let xyzPath = "";
+let useHash = false;
+let xyzFiles: Map<string, File> | null = null;
+let startSpecies = new Array<string>();
+let databaseURL = "http://10.42.144.124:43070";
+
+let databaseList: Promise<string[]> = fetch(databaseURL + "/api/database_list")
+  .then((response) => response.json())
+  .then((data) => {
+    return data["database_list"];
+  });
+
+async function loadGraph(graphName: string) {
+  const created = await fetch(`${databaseURL}/api/create_graph/${graphName}`, {
+    method: "POST",
+  });
+  if (!created.ok) {
+    console.error("Failed to create graph");
+    return;
+  }
+
+  graph = await fetch(`${databaseURL}/${graphName}/${graphName}/graph.json`)
+    .then((response) => response.text())
+    .then((text) => {
+      const graph = createGraphFromString(text);
+      useHash = true;
+      xyzPath = `${databaseURL}/${graphName}/`;
+      return graph;
+    });
+}
+</script>
+
+<h1>Autograph</h1>
+
+{#if !loadedGraph}
+  <div class="page">
+    <div>
+      <div>
+        Select a dataset to explore:
+        <select
+          on:change={(event) => {
+            const value = event.target.value;
+            if (!value) return;
+            loadGraph(value);
+          }}
+        >
+          <option value="">Select a dataset</option>
+          {#await databaseList then databases}
+            {#each databases as database}
+              <option value={database}>{database}</option>
+            {/each}
+          {:catch error}
+            <option value="">Error loading databases</option>
+          {/await}
+        </select>
+      </div>
+    </div>
+    <div>
+      <button
+        on:click={() => {
+          if (graph) {
+            loadedGraph = true;
+          }
+        }}
+      >
+        Explore
+      </button>
+    </div>
+  </div>
+{:else}
+  <GraphElement {graph} {xyzPath} {useHash} {xyzFiles} {startSpecies} />
+{/if}
+
+<style>
+@font-face {
+  font-family: "Quicksand-Bold";
+  src:
+    url("/fonts/Quicksand-Bold.woff2") format("woff2"),
+    url("/fonts/Quicksand-Bold.woff") format("woff"),
+    url("/fonts/Quicksand-Bold.ttf") format("truetype");
+
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Quicksand";
+  src:
+    url("/fonts/Quicksand-Regular.woff2") format("woff2"),
+    url("/fonts/Quicksand-Regular.woff") format("woff"),
+    url("/fonts/Quicksand-Regular.ttf") format("truetype");
+
+  font-weight: normal;
+  font-style: normal;
+}
+
+* {
+  font-family: "Quicksand", sans-serif;
+  font-size: 1.2rem;
+}
+
+.page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f0f0f0;
+  gap: 1rem;
+}
+
+h1 {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  margin: 15px;
+  padding: 0px;
+  font-size: 2em;
+  font-family: "Quicksand-Bold", sans-serif;
+  font-weight: 400;
+  color: #000000;
+  z-index: 100;
+  pointer-events: none;
+}
+
+.ownDataset {
+  width: 100%;
+}
+
+.center {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.center input {
+  width: 40%;
+}
+
+.center label {
+  display: inline-block;
+  margin: 0px;
+  padding: 0px;
+  width: 40%;
+  text-align: right;
+  padding-right: 0.5rem;
+}
+</style>
